2a20d1d774690b85fd394e7ab5c8c2c0e33a3fb1
catacretu <catalinalexandrucretu@gmail.com>
2025-08-30 00:18:16 +0300

    Add splash screen background&icon

--- app/src/main/java/com/example/ecommercefoodappcompose/ui/screens/SplashScreen.kt
+++ app/src/main/java/com/example/ecommercefoodappcompose/ui/screens/SplashScreen.kt
@@ -4,17 +4,21 @@ import android.view.animation.OvershootInterpolator
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.paint
 import androidx.compose.ui.draw.scale
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.example.ecommercefoodappcompose.R
 import kotlinx.coroutines.delay
 
@@ -39,24 +43,28 @@ fun SplashScreen(onNavigate: () -> Unit) {
         onNavigate()
     }
 
-    // Image
-
     Box(
         contentAlignment = Alignment.Center,
         modifier = Modifier
             .fillMaxSize()
-            .paint(
-                // TODO - dummy values
-                painterResource(id = R.drawable.ic_launcher_background),
-                contentScale = ContentScale.Crop,
-                alignment = Alignment.BottomEnd
+            .background(
+                brush = Brush.radialGradient(
+                    colors = listOf(
+                        Color(0xFF36693E).copy(alpha = 0.9f),
+                        Color(0xFF4CAF50).copy(alpha = 0.8f)
+                    ),
+                    center = Offset.Infinite,
+                    radius = 1000f
+                )
             )
 
     ) {
         Image(
-            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            painter = painterResource(id = R.drawable.ic_splash_cart),
             contentDescription = "AppIcon",
-            modifier = Modifier.scale(scale.value)
+            modifier = Modifier
+                .size(150.dp)
+                .scale(scale.value)
         )
     }
 }
