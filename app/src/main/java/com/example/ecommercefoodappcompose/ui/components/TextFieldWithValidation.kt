3fdae0c965683fb8b62f2223c47e99fcf4222842
catacretu <catalinalexandrucretu@gmail.com>
2025-08-30 22:15:21 +0300

    Resolve error padding - textInput

--- app/src/main/java/com/example/ecommercefoodappcompose/ui/components/TextFieldWithValidation.kt
+++ app/src/main/java/com/example/ecommercefoodappcompose/ui/components/TextFieldWithValidation.kt
@@ -1,14 +1,17 @@
 package com.example.ecommercefoodappcompose.ui.components
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun TextFieldWithValidation(
@@ -38,7 +41,8 @@ fun TextFieldWithValidation(
                 Text(
                     text = errorMessage,
                     color = MaterialTheme.colorScheme.error,
-                    style = MaterialTheme.typography.bodySmall
+                    style = MaterialTheme.typography.bodySmall,
+                    modifier = Modifier.padding(bottom = 10.dp)
                 )
             }
         },
